ab0cd11bdd8ff28bd6a8b4513b2ac777b2cb446d
hama1219 <touhougengakukyou@gmail.com>
2025-03-30 12:20:31 +0900

    フォント定義訂正

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,19 @@ import { setupCounter } from './counter.ts'
   const userAgent = navigator.userAgent;
 
   if (userAgent.includes("Edg") || userAgent.includes("Trident") || userAgent.includes("MSIE")) {
-      document.body.innerHTML = "<h2 class='root'>このページはEdgeまたはInternet Exploerでは表示できません<br /> GoogleChrome、FirefoxまたはSafari等でのご利用をお願いいたします。</h2>";
+      document.body.innerHTML =  `
+      <script>
+      (function(d) {
+        var config = {
+          kitId: 'top6hpz',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    </script>
+      <h2 class='root'>このページはEdgeまたはInternet Exploerでは表示できません<br /> GoogleChrome、FirefoxまたはSafari等でのご利用をお願いいたします。</h2> 
+ `;
       setTimeout(() => {
           window.location.href = "https://www.google.com/intl/ja_jp/chrome/";
       }, 5000);
