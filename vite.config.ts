fef88a7c19f8cf6d04d31a75faeb85320905a7fd
hama1219 <touhougengakukyou@gmail.com>
2025-11-09 20:31:27 +0900

    構成変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
-
 export default defineConfig({
+  root:'project', //プロジェクトフォルダをproject二設定
   base: './', // アセットパスを相対パスにする
+   publicDir: 'public',
+   envDir: '../',
 });
