9794b726bcd613bdd6c02840ce928387ed60b1ed
hama1219 <touhougengakukyou@gmail.com>
2025-11-09 22:38:06 +0900

    CI整備

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'node:path'
 import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root:'project', //プロジェクトフォルダをproject二設定
   base: './', // アセットパスを相対パスにする
    publicDir: 'public',
    envDir: '../',
+     build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, '20251122reminise/index.html'),
+      },
+    },
+  },
 });
